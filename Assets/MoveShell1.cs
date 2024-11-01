072a47dae2cea1abceaefba1f6dde26f10181f21
EntityEnjoyer <angelafsmartins7@gmail.com>
2024-11-01 17:58:01 +0000

    Acceleration
    Acceleration tutorial done

--- Assets/MoveShell1.cs
+++ Assets/MoveShell1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveShell : MonoBehaviour
+public class MoveShell1 : MonoBehaviour
 {
     float speed = 1.0f;
 
